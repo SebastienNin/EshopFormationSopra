4e22246bdcc7fa55e4e93ce5517ff7424faec53d
SebastienNin <home-world@hotmail.Fr>
2023-09-07 15:40:54 +0200

    remove idao client and update springapplication

--- src/main/java/eshop/formation/dao/IDAOClient.java
+++
@@ -1,9 +0,0 @@
-package eshop.formation.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mysql.cj.xdevapi.Client;
-
-public interface IDAOClient extends JpaRepository<Client,Integer> {
-
-}
--- src/test/java/eshop/formation/test/SpringApplication.java
+++ src/test/java/eshop/formation/test/SpringApplication.java
@@ -3,12 +3,13 @@ package eshop.formation.test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import eshop.formation.config.AppConfig;
+import junit.framework.Test;
 
 public class SpringApplication {
 	
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
-//		ctx.getBeanFactory().createBean(Test.class).run();
+		ctx.getBeanFactory().createBean(PopulateTest.class).run();
 		ctx.close();
 	}
 
