5ce4b43abbed31d4cbd56e326576f942f84dd729
SebastienNin <home-world@hotmail.Fr>
2023-09-07 15:38:22 +0200

    Move properties

--- src/test/java/eshop/formation/test/PopulateTest.java
+++ src/test/java/eshop/formation/test/PopulateTest.java
@@ -1,11 +1,24 @@
 package eshop.formation.test;
 
-import org.junit.Test;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import eshop.formation.dao.IDAOAdresse;
+import eshop.formation.model.Adresse;
 
 public class PopulateTest {
 
-	@Test
-	public void populate() {
-		
+	@Autowired
+	IDAOAdresse daoAdresse;
+	
+	public void run(String ...args) {
+		Optional<Adresse> opt = daoAdresse.findById(1);
 	}
+	
+//	@Test
+//	public void populate() {
+//		
+//		
+//	}
 }
---
+++ src/test/java/eshop/formation/test/SpringApplication.java
@@ -0,0 +1,15 @@
+package eshop.formation.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import eshop.formation.config.AppConfig;
+
+public class SpringApplication {
+	
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+//		ctx.getBeanFactory().createBean(Test.class).run();
+		ctx.close();
+	}
+
+}
